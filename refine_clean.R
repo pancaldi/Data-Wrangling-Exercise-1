b09a77a7e8381a1f4409c24d411fb6572003508b
Jacques Bikoundou <jaqo59@gmail.com>
2017-05-21 21:32:29 -0400

    Update refine_clean.R

--- refine_clean.R
+++ refine_clean.R
@@ -1,49 +1,50 @@
+
 # load data
 
 library(tidyr)
+library(dummies)
+library(stringdist)
 pp <- read_csv("refine_original"/docs.google.com/open?id/data)
 tbl_df(pp) 
 
 #clean up brand names
 
-clean_companies <- pp %>%
-  mutate(company = tolower(company)) %>%
-  mutate(company = case_when(
-    .$company %in% c("phillips", "phillps", "fillips", "phlips" , "phllips" , "phillipS") ~ "philips"
-    .$company %in% c("akz0", "ak zo" , "AKZO", "Akzo") ~ "akzo"
-    .$company %>%  c("Van Houten" , "van Houten") ~ "van houten"
-    .$company %in% c("Unilever", "unilver") ~ "unilever",
-    TRUE ~ .$company)
-  )
+closematch <- function(x, y) {
+wt <- c()
+  for (i in 1:length(y)) {
+    wt[i] <- stringdist(x, y[i])
+    }
+  which.min(wt)
+  return(y[which.min(wt)])
+  }
+brandName <- c("philips", "akzo", "van houten", "unilever")
+df&company <- sapply(df$company, closeMatch, y = brandName)
 
 #separate product code and number
 
-separate_product <- clean_companies %>%
-  separate('product code / number' , c("product_code", "product_number"), sep= "_",
-           remove = TRUE) 
+colnames(df)[which(names(df) == "product.code...number")] <- "productcode_number"
+df <- separate(df, productcode_number, c("productcode", "number"), sep = "_", remove = FALSE)
 
 #add product categories
 
-product_categories <- separate_product %>%
-  mutate(category = case_when(
-    .$product_code %in% c("p") ~ 'smartphone'
-    .$product_code %in% c("v") ~ 'TV'
-    .$product_code %in% c("x") ~ 'laptop'
-    .$product_code %in% c("q") ~ 'tablet'
-    TRUE ~ .$product_code))  
+productcode <- c("p", "v", "x", "q")
+productcategory <- c("Smartphone", "TV", "Laptop", "Tablet")
+prodcatMatch <- df(productcode, productcategory)
+df <- left_join(df, prodcatMatch)
 
 #add full address for geocoding
 
 full_address <- paste(df$address, df$city, df$country, sep = ",") 
-df <- cbind(df, full-address)
+df <- cbind(df, full_address)
 
 #create dummy variables for company & product category
 
-dummy_variables <- combined_address %>%
-mutate(company_binary = 1, product_binary = 1) %>%
-mutate(company = past0("company_", company), category = paste0(product_", category)) %>%
-spread(company, company_binary, fill = 0) %>%
-spread(category, product_binary, fill = 0) 
+company <- gsub(" ", "_", df$company)
+df2 <- dummy(company, sep = "_")
+df <- cbind(df, df2)
+product <- tolower(df$productcategory)
+df3 <- dummy(product, sep = "_")
+df <- cbind(df, df3)
 
 #save the output as a CSV file & submit to github
 
